fce6d6daed146ed728cbdb49ebe98e81fc5de33b
kshivam1177 <shivam@ivypods.com>
2021-12-24 16:41:10 +0530

    Work in Progress

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -5,7 +5,7 @@
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:video_player_web/video_player_web.dart';
+import 'package:yc_video_player_web/video_player_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
